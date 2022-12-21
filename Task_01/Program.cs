cad2971b247fe0fa60fe87748a25a88488877ccf
Einzel <einzel0102@mail.ru>
2022-12-21 20:17:06 +0800

    задача 1

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿int MaxDigit(int argument)
+{
+    int argument1 = argument / 10;
+    int argument2 = argument % 2;
+    int result = argument1;
+    
+    if(argument1 > argument2)
+    {
+        result = argument1;
+    }
+    else 
+    {
+        result = argument2;
+    }
+    return result;
+}
+
+int number = new Random(). Next(10,100);
+
+Console.WriteLine($"случайно число из отрезка 10-99 => {number}");
+
+
+int maxdigit = MaxDigit(number);
+Console.WriteLine(maxdigit);
+
+
+
+
+
+
+
+
